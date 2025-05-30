dad6b49e9fceaa66f25e1944047a0dcf2f6fbcb5
UnluckyLuki <themrluki43@gmail.com>
2025-05-30 10:15:10 +0200

    Updated phone page with crud

--- src/app/pages/phone.page/phone.page.ts
+++ src/app/pages/phone.page/phone.page.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Phone} from '../../models/Phone';
+import {PhoneService} from '../../services/phone.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-phone.page',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './phone.page.html',
   styleUrl: './phone.page.css'
 })
 export class PhonePage {
 
+  phones: Phone[] = [];
+  phone: Phone = {phoneId: 0, phoneNumber: '', phoneName: ''};
+  ePhone: Phone = {phoneId: 0, phoneNumber: '', phoneName: ''};
+  @ViewChild('editModal') editModal : ElementRef | undefined;
+
+  constructor(private service: PhoneService) {
+  }
+
+  ngOnInit() {
+    this.getAllPhones();
+  }
+
+  getAllPhones() {
+    this.service.getAll()
+      .subscribe(data => this.phones = data);
+  }
+
+  addPhone() {
+    this.service.create(this.phone)
+      .subscribe(() => this.getAllPhones());
+  }
+
+  openModal(phone: Phone){
+    if(this.editModal){
+      this.ePhone = {...phone};
+      this.editModal.nativeElement.style.display = 'block';
+    }
+  }
+
+  closeModal(){
+    if(this.editModal){
+      this.editModal.nativeElement.style.display = 'none';
+    }
+  }
+
+  editPhone() {
+    this.service.update(this.ePhone.phoneId, this.ePhone)
+      .subscribe(() => this.getAllPhones());
+  }
+
+  deletePhone(id: number) {
+    const isDelete = confirm('Are you sure you want to delete this phone?');
+    if(isDelete){
+      this.service.delete(id)
+        .subscribe(() => this.getAllPhones());
+    }
+  }
 }
