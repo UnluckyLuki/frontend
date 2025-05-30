470218bf0dde316c82392b46073a95fac98b37d9
UnluckyLuki <themrluki43@gmail.com>
2025-05-30 10:19:06 +0200

    Repaired routes and http config as well as cleared index (angular template)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {PhonePage} from './pages/phone.page/phone.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'phonePage',
+    component: PhonePage
+  }
+];
